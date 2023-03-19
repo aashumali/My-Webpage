4b78df61f6c5b1cd0b7665b800ef62a75d16e810
Ashwini Mali <128283008+aashumali@users.noreply.github.com>
2023-03-19 22:57:38 +0530

    Add files via upload

---
+++ cheatsheet.py
@@ -0,0 +1,128 @@
+num = 12
+for  i in range(1,11):
+   print(num,'x',i,'=',num*i)
+L=[4,5,6,7]
+print(all(L))
+L=[0,0,False]
+print(all(L))
+print Sunflower
+print(F'{127803:^20c}')
+
+abs function
+a = 23
+b = 45
+c = 56.78
+d = 5+5j
+print(abs(a))
+print(abs(b))
+print(abs(c))
+print(abs(d))
+
+all()function
+l = [4,5,6]
+print(all(l))
+
+l = ['aashu','dishu','vishu']
+print(all(l))
+
+l = [1, 0, False]
+print(all(l))
+bin()
+x = 10
+y = bin(x)
+print(y)
+
+class quantity:
+apple = 32
+banana = 43
+grapes = 44
+def function():
+ bool
+
+rows = int (input ('enter number of rows:'))
+for i in range(rows):
+    for j in range(i+1):
+      print("*",end="")
+      print("\
+
+a = 34
+b = 34
+print("the sum of a and b is",a + b)
+
+a = 34
+ b = 34
+ a>b
+ a > b
+a = input ("enter first number:")
+b = input ("enter second number:")
+a = int(a)
+b = int(b)
+avg = (a +b )/2
+ print("the average of a + b is", avg)
+'''to calculate square of a number entered by the user'''
+greeting = "good morning"
+name = "aashu"
+print(type(name))
+concatenating two strings
+c = gretting + name
+print(c)
+name = "aashu"
+k print(name[3])
+print(name[0:3])
+print(name[1:4])
+story = "once upon a time there were lived a king in a village."
+print(len(story))
+print(story.count("o"))
+ print(story.endswitch)
+print(story.capitalize())
+print(story.replace("once","aashu"))
+story = "aashu is good.\nhe\tis ve\ry good"
+print(story)
+print("good morning")
+Name = input("enter your name")
+print("good afternoon" + Name)
+letter = ''' dear <|name|>,
+greeting from abc coding house.i am happy to tell you abiut your selection you are selected!
+
+you are discolified!
+   
+date: <|date|>
+'''
+name =  input("enter your name\n")
+date = input("enter date\n")
+letter = letter.replace("<|name|>",name)
+letter = letter.replace("<|date|>",date )
+print(letter)
+ 
+ detect double space in a string
+st = "this is a string with double spaces"
+st = st.replace(" "," ")
+print(st)
+
+letter = "dear aashu, this is your laptop!" 
+print(letter)
+formatted_letter = "dear aashu,\n\tthis python course is nice!\nthanks!"
+print(formatted_letter)
+list and tuple
+a = [3,5,6,76]
+ print(a)
+print(a[3])
+print(a[3:2])
+fruits =["aash","dishu","vishu"]
+print(fruits)
+list slicing
+friends = ["harry","aashu","deep"]
+print(friends[0:4])
+print(friends[-2:])
+
+
+
+list = [2,4,6,2,3,33]
+print(list)
+list.reverse()
+list.sort()
+list.remove()
+list.append(34)
+list.insert(0,45)
+list = [2,4,6,2,3,33]
+list.pop(2)
